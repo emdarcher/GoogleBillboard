532def2aa2431d9302f0b7074c3f95e4f2963960
Ethan Durrant <emd519@yahoo.com>
2014-12-02 12:52:37 -0800

    working on it

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -24,30 +24,31 @@ public void draw()
 	for(int i=2;i<(e.length() -10);i++){
 		String digits = e.substring(i,i+10);
 		long dNum = Long.parseLong(digits);
-		if(isPrime(dNum)){
+		if(isPrime(dNum)==true){
 			System.out.println(dNum);
 		}
 	}
 }  
 
 public boolean isPrime(long num){
-  boolean out=false;
-  //if(((num & (long)(1<<63))!=0)||(num==1)||(num==0)){
+  boolean out=true;
+  if(((num & (long)(1<<63))!=0)||(num==1)||(num==0)){
     //checks if minus bit is set or number = 1 or 0
-    //return false;
-  //}
-  if(num<2){return false;}
+    return false;
+  }
+  //if(num<2){return false;}
   //else if(((num & ~((long)0x2))==0)||((num & ~((long)0x3))==0)){ //if num==2 or 3
   //  return true;
   //}
-  //else if(((num & (long)(1<<0))!=0) && (num % 3 !=0 )) {
+  else if(((num & (long)(1<<0))!=0) && (num % 3 !=0 )) {
     // if the number is odd continue
     // (checks if bit 0 is set, if it is then it's odd)
     // also checks if not divisible by 3
     for(long i=2;(i*i)<=num;i++){
-       out = ((num % i) == 0 ) ? false : true;
+       out = ((num % i) == 0 ) ? false : out;
+       //if(num%i ==0){return false;}
     }
-  //}
+  }
   return out;
 }
   static public void main(String[] passedArgs) {
