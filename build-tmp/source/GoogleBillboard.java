a7f199448811f8a9e03c97c32c87795f4c5ae23a
Ethan Durrant <emd519@yahoo.com>
2014-12-02 13:58:20 -0800

    cleaning stuff up

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -14,13 +14,14 @@ import java.io.IOException;
 
 public class GoogleBillboard extends PApplet {
 
-public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
-public void setup()  
-{            
+public final static String e = 
+"2.718281828459045235360287471352662497757247093"
++ "699959574966967627724076630353547594571382178"
++ "525166427427466391932003059";  
+public void setup(){            
      noLoop();  
 }  
-public void draw()  
-{   
+public void draw()  {   
 	for(int i=2;i<(e.length() -10);i++){
 		String digits = e.substring(i,i+10);
 		long dNum = Long.parseLong(digits);
